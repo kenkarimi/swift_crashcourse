fd3fccd73a90d3774ae758b5db307fe0ff2d19a1
Kennedy Mugera Karimi <kennedymugera@gmail.com>
2021-06-24 16:30:40 +0300

    typealias.swift finished

---
+++ libraries_frameworks.swift
@@ -0,0 +1,11 @@
+import Foundation
+import StoreKit
+import UIKit
+
+let button = UIButton();
+
+/*This file doesn't compile because StoreKit & UIKit
+  aren't really parts SwiftForWindows package we're using to learn Swift
+  on a windows platform.
+*/
+
---
+++ typealias.swift
@@ -0,0 +1,42 @@
+import Swift;
+
+//Typealias
+//A way to create your own types. Reason this exists is for more complex types that might be confusing to follow.
+
+
+//Example of a messy looking dictionary.
+/*
+var address: [String: String] = {"": ""}
+
+func validateAddress(address: [String: String]) {
+
+}
+
+validateCode(address: address);
+*/
+
+//Solution:
+
+typealias Address = [String: String]
+
+var address: Address = ["": ""]
+
+func validateAddress(address: Address) {
+
+}
+
+validateAddress(address: address);
+
+//Example 2:
+
+/*var validateFunction: ((String) -> Bool)?
+
+func didEnterCode(code: String) {
+    let result: validateFunction?(code)
+}*/
+
+//Solution:
+typealias PromoCodeValidationFunction = ((String) -> Bool)?
+
+var validationFunction: PromoCodeValidationFunction
+
